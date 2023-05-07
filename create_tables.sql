23f8c18d98d6a70ce95e561c4d64e6563f80bd24
Marco Nakasima <13857701+MNakasima@users.noreply.github.com>
2023-05-07 18:07:12 -0300

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,53 +1,53 @@
-create external table address 
+CREATE EXTERNAL TABLE address 
 (AddressID int, AddressLine1 string, AddressLine2 string, City string, PostalCode string, rowguid string, ModifiedDate string) 
 row format delimited fields terminated by ';' 
 location '/atividade_2/address/';
 
-create external table customer 
+CREATE EXTERNAL TABLE customer 
 (CustomerID int, Title string, Suffix string, CompanyName string, SalesPerson string, EmailAddress string, PasswordHash string, 
 PasswordSalt string, rowguid string, ModifiedDate string) 
 row format delimited fields terminated by ';' 
 location '/atividade_2/customer/';
 
-create external table customeraddress 
+CREATE EXTERNAL TABLE customeraddress 
 (CustomerID int, AddressID int, rowguid string, ModifiedDate string) 
 row format delimited fields terminated by ';' 
 location '/atividade_2/customeraddress/';
 
-create external table product 
+CREATE EXTERNAL TABLE product 
 (ProductID int, ProductNumber string, Color string, StandardCost float, ListPrice float, Size int, Weight int, ProductCategoryID int, 
 ProductModelID int, SellStartDate string, SellEndDate string, DiscontinuedDate string, ThumbNailPhoto string, 
 ThumbnailPhotoFileName string, rowguid string, ModifiedDate string) 
 row format delimited fields terminated by ';' 
 location '/atividade_2/product/';
 
-create external table productcategory 
+CREATE EXTERNAL TABLE productcategory 
 (ProductCategoryID int, ParentProductCategoryID int, rowguid string, ModifiedDate string) 
 row format delimited fields terminated by ';' 
 location '/atividade_2/productcategory/';
 
-create external table productdescription 
+CREATE EXTERNAL TABLE productdescription 
 (ProductDescriptionID int, Description string, rowguid string, ModifiedDate string) 
 row format delimited fields terminated by ';' 
 location '/atividade_2/productdescription/';
 
-create external table productmodel 
+CREATE EXTERNAL TABLE productmodel 
 (ProductModelID int, CatalogDescription string, rowguid string, ModifiedDate string) 
 row format delimited fields terminated by ';' 
 location '/atividade_2/productmodel/';
 
-create external table productmodeldescription 
+CREATE EXTERNAL TABLE productmodeldescription 
 (ProductModelID int, ProductDescriptionID int, Culture string, rowguid string, ModifiedDate string) 
 row format delimited fields terminated by ';' 
 location '/atividade_2/productmodeldescription/';
 
-create external table salesorderdetail 
+CREATE EXTERNAL TABLE salesorderdetail 
 (SalesOrderID int, SalesOrderDetailID int, OrderQty int, ProductID int, UnitPrice float, UnitPriceDiscount float, 
 LineTotal string, rowguid string, ModifiedDate string) 
 row format delimited fields terminated by ';' 
 location '/atividade_2/salesorderdetail/';
 
-create external table salesorderhead 
+CREATE EXTERNAL TABLE salesorderhead 
 (SalesOrderID int, RevisionNumber int, OrderDate string, DueDate string, ShipDate string, Status string, SalesOrderNumber string, 
 CustomerID int, ShipToAddressID int, BillToAddressID int, ShipMethod string, CreditCardApprovalCode int, SubTotal float, 
 TaxAmt float, Freight float, TotalDue float, Comment string, rowguid string, ModifiedDate string) 
