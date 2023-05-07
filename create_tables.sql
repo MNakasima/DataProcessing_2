c37cfd6dcdd4d3f1cd07bae98656764aa6515c79
Marco Nakasima <13857701+MNakasima@users.noreply.github.com>
2023-05-07 18:06:07 -0300

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -53,3 +53,15 @@ CustomerID int, ShipToAddressID int, BillToAddressID int, ShipMethod string, Cre
 TaxAmt float, Freight float, TotalDue float, Comment string, rowguid string, ModifiedDate string) 
 row format delimited fields terminated by ';' 
 location '/atividade_2/salesorderhead/';
+
+CREATE TABLE servico_contratados as
+SELECT p.productnumber as numero_produto,sd.orderqty as Quantidade, p.listprice as preco_unidade, sd.unitpricediscount as Desconto,
+pm.catalogdescription as Descricao
+FROM salesorderdetail sd
+INNER JOIN product p ON sd.productid = p.productid
+INNER JOIN productmodel pm ON p.productmodelid = pm.productmodelid;
+
+CREATE TABLE analise_cliente as
+SELECT c.salesperson as Pessoa, c.companyname as Companhia, a.city as Cidade FROM customeraddress ca
+INNER JOIN customer c ON ca.customerid = c.customerid
+INNER JOIN address a ON ca.addressid = a.addressid;
